47392b4254fafe2e9cb135772581ce4ee934edaa
Barne Kleinen <barne.kleinen@htw-berlin.de>
2023-02-24 23:48:12 +0100

    skip errors

--- find_tags3_10.py
+++ find_tags3_10.py
@@ -2,29 +2,33 @@ import sys
 # new in python 3.11, use other library for pyscript:
 # import tomllib
 import tomlkit
+
 toml_lib = tomlkit
 tags = ["minimal", "blog", "bootstrap"]
 tags_themes_file = 'theme_tags.toml'
 
+
 def find_tags(tags):
+    if len(tags) == 0:
+        return []
     tags_themes = read_list()
-    theme_lists = [tags_themes.get(tag, default= []) for tag in tags]
+    theme_lists = [tags_themes.get(tag, default=[]) for tag in tags]
     theme_sets = [set(theme_list) for theme_list in theme_lists]
-    if len(theme_sets) < 2:
-        return theme_sets
-    else:
-        head = theme_sets[0]
-        tail = theme_sets[1:]
-        result = head.intersection(*tail)
-        return list(result)
+
+    head = theme_sets[0]
+    tail = theme_sets[1:]
+    result = head.intersection(*tail)
+    return list(result)
+
 
 def read_list(file=tags_themes_file):
     with open(file, "r+b") as f:
         tags_themes = toml_lib.load(f)
         return tags_themes
 
+
 def find_themes_with_tags(tag_str):
-    tags = [ t.strip() for t in tag_str.split(",")]
+    tags = [t.strip() for t in tag_str.split(",")]
     result = find_tags(tags)
     result_urls = [f'https://{l}' for l in result]
     return "\n".join(result_urls)
@@ -32,4 +36,5 @@ def find_themes_with_tags(tag_str):
 
 if __name__ == '__main__':
     r = find_themes_with_tags("bootstrap, blog, minimal")
+    r = find_themes_with_tags("bootstrap")
     print(r)
